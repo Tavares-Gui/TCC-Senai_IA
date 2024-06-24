a2af0d0806a93daa166f9e2473ac2a39dfbd4a46
Guilherme Tavares <guilherme.rtavares@outlook.com>
2024-06-24 11:04:05 -0300

    cleaning and training

--- IA.py
+++ IA.py
@@ -3,16 +3,41 @@ import string
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from nltk.corpus import stopwords
 from nltk.probability import FreqDist
 from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer, RSLPStemmer
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+# nltk.download('rslp')
+# nltk.download('punkt')
+# nltk.download('stopwords')
+# nltk.download('vader_lexicon')
 
 file_path = './PerguntasRespostas.csv'
 data = pd.read_csv(file_path)
 
-remove_chars = "?.,;!:"
-
+remove_chars = "?.;!:"
 def clean_text(text):
     return ''.join(char for char in text if char not in remove_chars)
+
+stop_words = set(stopwords.words('portuguese'))
+def remove_stopwords(tokens):
+    return [word for word in tokens if word.lower() not in stop_words]
+
+lemmatizer = WordNetLemmatizer()
+stemmer = RSLPStemmer()
+
+def lemmatize_tokens(tokens):
+    return [lemmatizer.lemmatize(token) for token in tokens]
+
+def stem_tokens(tokens):
+    return [stemmer.stem(token) for token in tokens]
+
+sia = SentimentIntensityAnalyzer()
+
+def analyze_sentiment(text):
+    return sia.polarity_scores(text)
 
 all_questions = []
 perguntas = data['Perguntas']
@@ -20,21 +45,23 @@ perguntas = data['Perguntas']
 all_answers = []
 respostas = data['Respostas']
 
+apply_lemmatization = True
+apply_stemming = True
+
 for text in perguntas:
     cleaned_questions = clean_text(text)
     questions = word_tokenize(cleaned_questions)
+    questions = remove_stopwords(questions)
+    if apply_lemmatization:
+        questions = lemmatize_tokens(questions)
+    if apply_stemming:
+        questions = stem_tokens(questions)
     all_questions.extend(questions)
 
-for text in respostas:
-    answers = word_tokenize(text)
-    all_answers.extend(answers)
-
 fdq = FreqDist(all_questions)
-fda = FreqDist(all_answers)
 
 print(fdq.most_common(10))
-print(fda.most_common(10))
 
-fdq.plot(30, cumulative=False)
-fda.plot(30, cumulative=False)
-plt.show()
+for i, text in enumerate(perguntas):
+    sentiment = analyze_sentiment(text)
+    print(f"Sentimento da Pergunta {i+1}: {sentiment}")

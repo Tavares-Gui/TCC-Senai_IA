02a1824cd97e5b3c139c261c2ea6630514b9e6fb
Emanuelly <Emanuellybs14@gmail.com>
2024-07-23 10:59:29 -0300

    model

--- training.py
+++ training.py
@@ -31,7 +31,7 @@ param_grid = {
     'n_estimators': [50, 100, 200],
     'max_depth': [None, 10, 20],
     'min_samples_split': [2, 5, 10],
-    'min_samples_leaf': [1, 2, 4]
+    'min_samples_leaf': [1, 5, 7]
 }
 
 rf = RandomForestClassifier(random_state=42)
